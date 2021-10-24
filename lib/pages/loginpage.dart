f0daa4bab355e5d1279a53d69fce85ce3f5bd277
Udit-Kansal <uditkansal2002@gmail.com>
2021-10-25 01:28:00 +0530

    commit day6

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -3,7 +3,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool statechange = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -11,14 +19,16 @@ class LoginPage extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              SizedBox(height: 30,),
+              SizedBox(
+                height: 30,
+              ),
               Image.asset(
                 "assets/images/login_image.png",
               ),
               SizedBox(
                 height: 30.0,
               ),
-              Text("Welcome",
+              Text("Welcome $name",
                   style: TextStyle(
                     fontSize: 26,
                     fontWeight: FontWeight.bold,
@@ -32,6 +42,10 @@ class LoginPage extends StatelessWidget {
                 child: TextFormField(
                   decoration: InputDecoration(
                       labelText: "Username", hintText: "Enter Username"),
+                  onChanged: (val) {
+                    name = val;
+                    setState(() {});
+                  },
                 ),
               ),
               SizedBox(
@@ -48,22 +62,53 @@ class LoginPage extends StatelessWidget {
               SizedBox(
                 height: 20,
               ),
-              ElevatedButton(
-                onPressed: () {
+              InkWell(
+                onTap: () async {
+                  setState(() {
+                    statechange = true;
+                  });
+                  await Future.delayed(Duration(seconds: 1));
                   Navigator.pushNamed(context, "/home");
                 },
-                
-                style: TextButton.styleFrom(minimumSize: Size(100, 40),
-                  shadowColor: Colors.grey
-                ),
-                child: Text(
-                  "Login",
-                  style: TextStyle(fontSize: 17),
+                child: AnimatedContainer(
+                  duration: Duration(seconds: 1),
+                  height: 50,
+                  width: statechange ? 50 : 130,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(statechange ? 25 : 8),
+                    color: Colors.deepPurple,
                   ),
-              )
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: statechange
+                        ? Icon(
+                            Icons.done,
+                            color: Colors.white,
+                          )
+                        : Text(
+                            "Login",
+                            style: TextStyle(
+                              fontSize: 18,
+                              //fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                  ),
+                ),
+              ),
+              // ElevatedButton(
+              //   onPressed: () {
+              //     Navigator.pushNamed(context, "/home");
+              //   },
+              //   style: TextButton.styleFrom(
+              //       minimumSize: Size(100, 40), shadowColor: Colors.grey),
+              //   child: Text(
+              //     "Login",
+              //     style: TextStyle(fontSize: 17),
+              //   ),
+              // )
             ],
           ),
-        )
-    );
+        ));
   }
 }
